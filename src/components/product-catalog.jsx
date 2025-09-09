6394ad6603108fd87d5909c56caec1690e5f9d4f
Kurt Guardia <kurtguardia@gmail.com>
2025-09-09 19:06:27 +0400

    fix(catalog) fix card width

--- src/components/product-catalog.jsx
+++ src/components/product-catalog.jsx
@@ -39,7 +39,7 @@ export function ProductCatalog({ productos, inventario }) {
           return (
             <Card
               key={product.id}
-              className='glass-effect rounded-2xl overflow-hidden flex flex-col w-1/4'
+              className='glass-effect rounded-2xl overflow-hidden flex flex-col w-ful md:w-1/4'
             >
               <CardContent className='p-6 flex flex-col flex-1 items-center text-center'>
                 <div className='relative w-32 h-32 mb-4'>
